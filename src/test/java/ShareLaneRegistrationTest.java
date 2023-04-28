c8246570987e56a2ebd20cd5c89ec07d47c836cd
Ryhor <ryhor,dubrovin@pragmaticplay.com>
2023-04-28 09:49:57 +0400

    master

--- src/test/java/ShareLaneRegistrationTest.java
+++ src/test/java/ShareLaneRegistrationTest.java
@@ -24,5 +24,41 @@ public class ShareLaneRegistrationTest {
         Assert.assertTrue(registerButton.isDisplayed(), "Register button isn't displayed");
     }
 
+    @Test
+    public void fourDigitsZipCodeTest() {
+        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver");
+        driver = new ChromeDriver();
+//        Open register page https://www.sharelane.com/cgi-bin/register.py
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+//        Input 4 digits to zip code field
+        driver.findElement(By.name("zip_code")).sendKeys("1234");
+//        Click Continue button
+        driver.findElement(By.cssSelector("[value='Continue']")).click();
+//        Check Continue button
+        WebElement continueButton = driver.findElement(By.cssSelector("[value='Continue']"));
+        Assert.assertTrue(continueButton.isDisplayed(), "Continue button isn't displayed");
+//        Check message
+        WebElement errorMessage = driver.findElement(By.className("error_message"));
+        Assert.assertTrue(errorMessage.isDisplayed(), "Error message isn't displayed");
+    }
+
+    @Test
+    public void sixDigitsZipCodeTest() {
+        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver");
+        driver = new ChromeDriver();
+//        Open register page https://www.sharelane.com/cgi-bin/register.py
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+//        Input 6 digits to zip code field
+        driver.findElement(By.name("zip_code")).sendKeys("123456");
+//        Click Continue button
+        driver.findElement(By.cssSelector("[value='Continue']")).click();
+//        Check Continue button
+        WebElement continueButton = driver.findElement(By.cssSelector("[value='Continue']"));
+        Assert.assertTrue(continueButton.isDisplayed(), "Continue button isn't displayed");
+//        Check message
+        WebElement errorMessage = driver.findElement(By.className("error_message"));
+        Assert.assertTrue(errorMessage.isDisplayed(), "Error message isn't displayed");
+    }
+
 
 }
